36d89b58f8b2b44627c321045d0b70fab4cd7a78
KomfortKimko <komfortkimk@gmail.com>
2024-07-07 13:00:44 +0300

    DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const changeParagraph = document.querySelector('p');
+    changeParagraph.innerHTML = 'This is really cool!';
+});
